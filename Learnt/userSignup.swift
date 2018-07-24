617a7ac2e5dbf98451fc1562ba41973cc309204c
calebscottdeflected <calebscott@me.com>
2018-07-24 16:47:28 -0500

    Sign up

---
+++ Learnt/userSignup.swift
@@ -0,0 +1,51 @@
+//
+//  userSignup.swift
+//  Learnt
+//
+//  Created by Caleb Scott on 7/24/18.
+//  Copyright © 2018 FloatMe. All rights reserved.
+//
+
+class userSignup: UIViewController {
+    @IBOutlet var email: UITextView!
+    @IBOutlet var password: UITextView!
+    @IBOutlet var confirm: UITextView!
+
+    @IBOutlet var signUpB: UIButton!
+    @IBAction func signUp(){
+        if email.text != "" && password.text != "" && confirm.text != "" {
+            if password.text == confirm.text {
+        Auth0
+            .authentication()
+            .createUser(
+                email: "support@auth0.com",
+                password: "secret-password1",
+                connection: "Username-Password-Authentication",
+                userMetadata: ["first_name": "First",
+                               "last_name": "Last"]
+            )
+            .start { result in
+                switch result {
+                case .success(let user):
+                    print("User Signed up: \(user)")
+                case .failure(let error):
+                    print("Failed with \(error)")
+                }
+        }
+            } else {
+                
+            }
+        } else {
+            
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+      
+    }
+    
+    
+}
