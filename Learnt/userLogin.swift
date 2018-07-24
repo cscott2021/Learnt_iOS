b2587c4602ea7b0a73b02287778b751079699776
calebscottdeflected <calebscott@me.com>
2018-07-24 16:34:12 -0500

    User login class creation

---
+++ Learnt/userLogin.swift
@@ -0,0 +1,20 @@
+//
+//  userLogin.swift
+//  Learnt
+//
+//  Created by Caleb Scott on 7/24/18.
+//  Copyright © 2018 FloatMe. All rights reserved.
+//
+
+import UIKit
+
+class userLogin: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+    
+    
+}
