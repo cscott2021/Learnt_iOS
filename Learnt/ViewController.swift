a985b099892d483463772325d1eab5b47159abf8
Maria McCready <Littlemac700@gmail.com>
2018-07-24 09:30:49 -0500

    The testing

--- Learnt/ViewController.swift
+++ Learnt/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("the test")
+        print("the testing")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
