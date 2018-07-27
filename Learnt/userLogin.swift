c98776572833f9bd9fcd3bbed6da1f7aae72d15b
calebscottdeflected <calebscott@me.com>
2018-07-27 16:27:05 -0500

    Auth0 Removal

--- Learnt/userLogin.swift
+++ Learnt/userLogin.swift
@@ -7,28 +7,14 @@
 //
 
 import UIKit
-import Auth0
+
 class userLogin: UIViewController {
     @IBOutlet var email: UITextField!
     @IBOutlet var password: UITextField!
     @IBOutlet var signIn: UIButton!
     @IBAction func login(){
         if email.text != "" && password.text != "" {
-        Auth0
-            .authentication()
-            .login(
-                usernameOrEmail: email.text!,
-                password: password.text!,
-                realm: "Username-Password-Authentication",
-                scope: "openid")
-            .start { result in
-                switch result {
-                case .success(let credentials):
-                    print("Obtained credentials: \(credentials)")
-                case .failure(let error):
-                    print("Failed with \(error)")
-                }
-        }
+
         }
     }
     override func viewDidLoad() {
--- Learnt/userSignup.swift
+++ Learnt/userSignup.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2018 FloatMe. All rights reserved.
 //
 import UIKit
-import Auth0
 class userSignup: UIViewController {
     @IBOutlet var email: UITextField!
     @IBOutlet var password: UITextField!
@@ -16,30 +15,8 @@ class userSignup: UIViewController {
     @IBAction func signUp(){
         if email.text != "" && password.text != "" && confirm.text != "" {
             if password.text == confirm.text {
-        Auth0
-            .authentication()
-            .createUser(
-                email: email.text!,
-                password: password.text!,
-                connection: "Username-Password-Authentication",
-                userMetadata: ["first_name": "First",
-                               "last_name": "Last"]
-            )
-            .start { result in
-                switch result {
-                case .success(let user):
-                    print("User Signed up: \(user)")
-                case .failure(let error):
-                    print("Failed with \(error)")
-                }
-        }
-            } else {
-                
             }
-        } else {
-            
-        }
-        
+    }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
