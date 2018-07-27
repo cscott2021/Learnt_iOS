ca7ed8f712f88860e8fac3033fd012257c3cde4c
calebscottdeflected <calebscott@me.com>
2018-07-27 15:57:01 -0500

    App Analytics

--- Learnt/AppDelegate.swift
+++ Learnt/AppDelegate.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 import Auth0
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
         return Auth0.resumeAuth(url, options: options)
+        
     }
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
