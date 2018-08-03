b184f3066ec7fe6d1d4740a43dc8c4a1334dbf6f
calebscottdeflected <calebscott@me.com>
2018-08-03 09:01:38 -0500

    Added query for tutors

--- Learnt/tutorOpen.swift
+++ Learnt/tutorOpen.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class tutorOpen {
+import Firebase
+public class tutorOpen: UIViewController {
      var tutor_ref: DatabaseReference = Database.database().reference(withPath: "open_tutors")
+    func startQuery() {
+        let query = Database.database().reference().child("open_tutors").queryLimited(toLast: 10)
+        
+        _ = query.observe(.childAdded, with: { [weak self] snapshot in
+
+            if  let data        = snapshot.value as? [String: String],
+                let id          = data["user_id"],
+                let name        = data["name"],
+                let subject        = data["subject"]
+            {
+                print(data)
+                channel_id = id
+                
+            }
+        })
+    }
+    
     
     
 }
