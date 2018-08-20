8077c9bb8da449dd6fafbc5ca2c15cf3f4ead84f
Christopher <cvillalobos05563@gmail.com>
2018-08-20 11:52:02 -0500

    drop shadow

--- Learnt/mainStudent.swift
+++ Learnt/mainStudent.swift
@@ -12,11 +12,18 @@ import UIKit
 import FirebaseUI
 import Firebase
 class mainStudent: UIViewController {
-   
+    @IBOutlet weak var shadowView: UIView!
     @IBOutlet weak var img: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        shadowView.layer.cornerRadius = 3
+        shadowView.layer.shadowColor = UIcolor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha:1.0).cgColor
+        shadowView.layer.shadowOFFset = CGSize(width: 0, height: 1.75)
+        shadowView.layer.shadowRadius = 1.7
+        shadowView.layer.shadowOpacity = 0.45
+         
         
         img.layer.cornerRadius = img.frame.size.width/2
         img.clipsToBounds = true
