45b9978ed4895fd60e8684b9dd5da059f57647fd
calebscottdeflected <calebscott@me.com>
2018-08-22 13:59:47 -0500

    Fix build

--- Learnt/waiting.swift
+++ Learnt/waiting.swift
@@ -12,17 +12,17 @@ import FirebaseUI
 import Firebase
 var wait: Bool!
 class waiting: UIViewController {
-    func runTimedCode(){
+    @objc func runTimedCode(){
         let ViewControllernew1 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "whiteboard") as UIViewController
         
         
-        self?.navigationController?.pushViewController(ViewControllernew1, animated: true)
+        self.navigationController?.pushViewController(ViewControllernew1, animated: true)
     }
  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        gameTimer = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: false)
+        _ = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: false)
 
        
         
