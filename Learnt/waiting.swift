2795db16c8bb35332a8da5f5211e0d50ebe8d832
calebscottdeflected <calebscott@me.com>
2018-08-22 13:56:40 -0500

    Temp fix build

--- Learnt/waiting.swift
+++ Learnt/waiting.swift
@@ -12,34 +12,19 @@ import FirebaseUI
 import Firebase
 var wait: Bool!
 class waiting: UIViewController {
-
+    func runTimedCode(){
+        let ViewControllernew1 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "whiteboard") as UIViewController
+        
+        
+        self?.navigationController?.pushViewController(ViewControllernew1, animated: true)
+    }
  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        wait = true
-        let query = Database.database().reference().child("need_tutor").child(user_id).queryLimited(toLast: 10)
-        
-        _ = query.observe(.childAdded, with: { [weak self] snapshot in
-            
-            if  let data        = snapshot.value as? [String: String],
-                let id          = data["user_id"],
-                let name        = data["name"],
-                let subject        = data["subject"],
-                let status       = data["status"]
-            {
-                print(data)
-                
-                if subject == "subject" && status == "closed" && wait == true {
-                    wait = false
-                    let ViewControllernew1 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "whiteboard") as UIViewController
-                    
-                    
-                    self?.navigationController?.pushViewController(ViewControllernew1, animated: true)
-                }
-                
-            }
-        })
+        gameTimer = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: false)
+
+       
         
     }
 }
